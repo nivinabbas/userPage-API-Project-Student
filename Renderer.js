e8ec856b8091d28addb226c92a286e087f6e37c5
nivinabbas <nivin.abbas1@gmail.com>
2021-11-26 23:02:22 +0200

    add the handlebar for pokemon and meat

--- Renderer.js
+++ Renderer.js
@@ -13,15 +13,33 @@ class Renderer {
     let newHTMLforUser = templateForUser(allData);
     $('.user-container').append(newHTMLforUser);
 
+    $('.friends-container').empty();
+
     let sourceForFriends = $('#friends-template').html();
     let templateForFriends = Handlebars.compile(sourceForFriends);
     let newHTMLforFriends = templateForFriends(allData);
     $('.friends-container').append(newHTMLforFriends);
+
+    $('.quote-container').empty();
 
     let sourceForQuote = $('#quote-template').html();
     let templateForQuote = Handlebars.compile(sourceForQuote);
     let newHTMLforQuote = templateForQuote(allData);
     console.log(allData);
     $('.quote-container').append(newHTMLforQuote);
+
+    $('.pokemon-container').empty();
+
+    var sourceForPokemon = $('#pokemon-template').html();
+    var templateForPokemon = Handlebars.compile(sourceForPokemon);
+    var newHTMLForPokemon = templateForPokemon(allData);
+    $('.pokemon-container').append(newHTMLForPokemon);
+
+    $('.meat-container').empty();
+
+    var sourceForMeal = $('#aboutMe-template').html();
+    var templateForMeal = Handlebars.compile(sourceForMeal);
+    var newHTMLForMeal = templateForMeal(allData);
+    $('.meat-container').append(newHTMLForMeal);
   }
 }
